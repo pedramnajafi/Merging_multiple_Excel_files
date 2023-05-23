f3e8cc25a3187cf7e3d7fe023a868bdd50223efc
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-05-23 13:59:01 +0330

    Update Merging Multiple Excel Files.py

--- Merging Multiple Excel Files.py
+++ Merging Multiple Excel Files.py
@@ -5,7 +5,6 @@ input_file_path = "E:/Pedram/Pedram/Folder/"
 output_file_path = "E:/Pedram/Pedram/Folder/New/"
 excel_file_list = os.listdir(input_file_path)
 
-
 df = pd.DataFrame()
 
 for excel_files in excel_file_list:
